37c2d876d035bbcb5fd01046353065e437d6a433
Ayuma <adityajatisari@gmail.com>
2025-07-06 22:46:49 +0700

    Prepare for Merge

---
+++ bridge/Map2/intro_map_2.gd
@@ -0,0 +1,14 @@
+extends Node2D
+signal dialog_finished(result: String)
+
+func ShowDialog():
+	if not Dialogic.signal_event.is_connected(_on_dialogic_signal):
+			Dialogic.signal_event.connect(_on_dialogic_signal)
+	Dialogic.start("IntroMap2")
+	
+
+func _on_dialogic_signal(argument: String):
+	SendSignal(argument)
+		
+func SendSignal(result: String):
+	emit_signal("dialog_finished", result)
